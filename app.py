15552881b27f4b8c208986b7c8cd86560b86ea2b
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2023-06-23 04:19:23 +0000

    Implemented Login/Register/Session/Logout/Dashboard

--- app.py
+++ app.py
@@ -1,10 +1,66 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, jsonify, flash
+from database import register_user_to_db, login_user
+import os
 
 app=Flask(__name__)
+
+app.secret_key = os.environ['SECRET_KEY']
+
 
 @app.route("/")
 def home():
   return render_template('home.html')
+
+
+##############################################################
+############### LOGIN / REGISTRATION / SESSION ###############
+##############################################################
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+        try:
+          data = request.form
+          user = login_user(data)[0]['username']
+          if user:
+              session['logged_in'] = True
+              session['username'] = user
+              return render_template('dashboard.html')
+        except TypeError:
+          flash('Invalid email or password')
+
+  return render_template('login.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+  if request.method == 'POST':
+    data = request.form
+    pw = data['password']
+    cpw = data['confirmed_password']
+    if pw == cpw:
+      register_user_to_db(data)
+      return redirect('/login')
+    else:
+      flash('Passwords do not match!')
+  return render_template('register.html')
+
+@app.route("/dashboard")
+def dashboard():
+  try:
+    if session['username'] != None:
+      return render_template('dashboard.html')
+  except:
+    return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+######################   END    ##############################
+############### LOGIN / REGISTRATION / SESSION ###############
+##############################################################
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
---
+++ database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+def register_user_to_db(data):
+  with engine.connect() as conn:
+    conn.execute(text("INSERT INTO users (email_address, username, password) VALUES (:email_address, :username, :password)").
+                 params(
+                   email_address = data['email_address'], 
+                   username = data['username'],     
+                   password = data['password']))
+
+
+def login_user(data):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM users WHERE email_address = :email_address AND password = :password").
+                 params(
+                   email_address = data['email_address'],     
+                   password = data['password']))
+    user = []
+    for row in result.all():
+      user.append(row._asdict())
+    if len(user) == 0:
+      return None
+    else:
+      return user
